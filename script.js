d694bdfead2bdaf42887b447af4be300ca326954
Rowan Oliver <rowanoliver@Rowans-MacBook-Pro.local>
2020-11-10 12:23:04 -0800

    beginning/all green

---
+++ script.js
@@ -0,0 +1,14 @@
+// things that are needed:
+// create button- created in HTML
+// make timer that starts when presented with question 
+// prompt user through questions and and answers
+// when user is incorrect reduce time on timer 
+// timer runs out when all questions are answered or when the timer runs out
+// store scores with initials on local storage 
+
+
+//make button clickable 
+<button onclick="click()"> Start button</button>
+
+setInterval(function() )
+
