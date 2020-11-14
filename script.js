624e6a7b455a5e1227afad6aea12ae2c05f9107f
Rowan Oliver <r.oliver.yoga@gmail.com>
2020-11-14 09:48:21 -0800

    starter

--- script.js
+++ script.js
@@ -4,11 +4,66 @@
 // prompt user through questions and and answers
 // when user is incorrect reduce time on timer 
 // timer runs out when all questions are answered or when the timer runs out
-// store scores with initials on local storage 
+// store scores with initials on lowocal storage 
 
 
 //make button clickable 
+generateBtn.addeventListener("click")
 <button onclick="click()"> Start button</button>
 
-setInterval(function() )
+var theTimer;
 
+function setInterval() {
+
+    theTimer = setInterval()
+}
+
+
+var quizQuestions ={
+    poistion: 0,
+    list: [
+        {
+            question: " What is an object?",    
+            choice: [
+                "A standalone enitiy",// correct answer
+                "A ball",
+                "A method",
+                "Google it",
+            ],
+                answer:0
+              
+        },
+        {
+            question:"What breaks a code in JavaScript?",
+            choices: [
+                "A very large error",
+                 "Feeding it after midnight",
+                 "A very,very small error",// correct answer
+                "Google it",
+
+            ]
+                answer: 2
+
+        },
+        {
+            question:" "
+        }       
+            
+            
+}
+
+
+
+
+
+
+
+
+
+// Questons:
+// What is an object? Answer: 
+// What breaks a code in JS? answer: some kind of puncuation
+// What is a variable? Answers: an empty container, a JS property, an allgrebreic term, anything that can vary
+// How long does it take to learn JavaScript? Answers: 1 month, 1 year, 10 years, when the light turns on
+// What is a string? Answer: fibrouris material, what causes a function, data respresented in text form, google it
+// What is an array? Answer: 
